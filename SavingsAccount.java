5818b4928a3cc9f705663726f66d643fcbcbd3dd
Eddie C. <edimc2000@gmail.com>
2025-11-01 03:55:03 -0500

    drafted all requirements

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -2,15 +2,17 @@ package OOP_A1;
 
 import static java.lang.System.out;
 
+import OOP_A1.OOP_A1.Helper;
+
 public class SavingsAccount {
 
   private String accountName;
-  private Double initialBalance;
-  private Double balance;
+  private double initialBalance;
+  private double balance;
   private final int ACCOUNT_NUMBER;
 
-  private static Double annualInterestRate = 0.00 ;
-  private static Double monthlyInterestRate = annualInterestRate / 12;
+  private static double annualInterestRate = 0.00;
+  private static double monthlyInterestRate = annualInterestRate / 12;
 
   public String getAccountName() {
     return accountName;
@@ -20,42 +22,78 @@ public class SavingsAccount {
     return ACCOUNT_NUMBER;
   }
 
-  public Double getInitialBalance() {
+  public double getInitialBalance() {
     return initialBalance;
   }
 
-  public Double getBalance() {
+  public double getBalance() {
     return balance;
   }
 
-  public static Double getMonthlyInterestRate() {
+  public static double getMonthlyInterestRate() {
     monthlyInterestRate = annualInterestRate / 12;
     return monthlyInterestRate;
   }
 
-  public static Double getAnnualInterestRate() {
+  public static double getAnnualInterestRate() {
     return annualInterestRate;
   }
 
-
-  public static void  setAnnualInterestRate(Double rate) {
+  public static void setAnnualInterestRate(double rate) {
     annualInterestRate = rate;
   }
 
-  SavingsAccount(String accountName, int accountNumber, Double initialBalance) {
+  public void setCurrentBalance(double amount) {
+    balance = amount;
+  }
+
+  SavingsAccount(String accountName, int accountNumber, double initialBalance) {
     this.accountName = accountName;
     this.ACCOUNT_NUMBER = accountNumber;
     this.initialBalance = initialBalance;
   }
 
   // overload
-  SavingsAccount(int accountNumber, Double initialBalance) {
+  SavingsAccount(int accountNumber, double initialBalance) {
     this.ACCOUNT_NUMBER = accountNumber;
     this.initialBalance = initialBalance;
   }
 
-  public Double addMonthlyInterest(Double balance){
-    return balance * getMonthlyInterestRate(); 
+  public double addMonthlyInterest(double balance) {
+    return balance * getMonthlyInterestRate();
+  }
+
+  public static void compareAccounts(SavingsAccount account1, SavingsAccount account2) {
+
+    account1.setCurrentBalance(account1.getInitialBalance());
+    account2.setCurrentBalance(account2.getInitialBalance());
+    double finalBalanceBothAccounts = 0;
+
+    StringBuilder display = new StringBuilder(
+        "Monthly balances for one year with " + SavingsAccount.getAnnualInterestRate() + " annual interest:\n\n");
+    display.append("Month\t\tAccount #\tBalance\t\tAccount #\tBalance\n");
+    display.append("-----\t\t---------\t-------\t\t---------\t-------\n");
+
+    for (int i = 0; i < 13; i++) {
+      display.append(String.format("%3d", i)); // Right-align: 3-char width
+      display.append("\t\t" + account1.ACCOUNT_NUMBER);
+      display.append("\t\t$ " + Helper.formatTwoDecimals(account1.getBalance()));
+
+      display.append("\t" + account2.ACCOUNT_NUMBER);
+      display.append("\t\t$ " + Helper.formatTwoDecimals(account2.getBalance()));
+
+      display.append("\n");
+      account1.setCurrentBalance(account1.getBalance() + account1.addMonthlyInterest(account1.getBalance()));
+      account2.setCurrentBalance(account2.getBalance() + account2.addMonthlyInterest(account2.getBalance()));
+
+      if (i == 11)
+        finalBalanceBothAccounts += account1.getBalance() + account2.getBalance();
+    }
+
+    display
+        .append("\nFinal balance of both accounts combined\t: $ " + Helper.formatTwoDecimals(finalBalanceBothAccounts));
+    out.println(display);
+
   }
 
 }
--- SavingsAccountDriver.java
+++ SavingsAccountDriver.java
@@ -6,24 +6,10 @@ public class SavingsAccountDriver {
 
     public static void main(String[] args) {
 
-
-        SavingsAccount saver1 = new SavingsAccount(10002, (double) 2000);
-        SavingsAccount saver2 = new SavingsAccount(10003, (double) 3000);
+        SavingsAccount saver1 = new SavingsAccount(10002, 2000);
+        SavingsAccount saver2 = new SavingsAccount(10003, 3000);
         SavingsAccount.setAnnualInterestRate(0.05);
 
-        out.println(saver1.getAccountNumber());
-        out.println(Helper.formatTwoDecimals(saver1.getInitialBalance()));
-        out.println(saver2.getAccountNumber());
-        out.println(Helper.formatfourDecimals(SavingsAccount.getMonthlyInterestRate()));
-        out.println(Helper.formatfourDecimals(SavingsAccount.getAnnualInterestRate()));
-        
-        // SavingsAccount.setAnnualInterestRate(0.10);
-        out.println(Helper.formatfourDecimals(SavingsAccount.getMonthlyInterestRate()));
-        out.println(Helper.formatfourDecimals(SavingsAccount.getAnnualInterestRate()));
-        out.println("----");
-        out.println(saver1.getInitialBalance());
-        out.println(saver1.getBalance());
-        out.println("----");
-        out.println(saver1.addMonthlyInterest((double)2000));
+        SavingsAccount.compareAccounts(saver1, saver2);
     }
 }
