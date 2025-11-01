6a53afda7719abc4e3643e2c28e3abf4f5625fda
Eddie C. <edimc2000@gmail.com>
2025-11-01 02:52:40 -0500

    draft 1-4

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,12 +1,63 @@
-
 package OOP_A1;
 
-import static java.lang.System.out; 
+import static java.lang.System.out;
 
 public class SavingsAccount {
 
-  out.println("tesu");
+  private String accountName;
+  private Double initialBalance;
+  private Double balance;
+  private final int ACCOUNT_NUMBER;
+
+  private static Double annualInterestRate = 0.05;
+  private static Double monthlyInterestRate = annualInterestRate / 12;
+
+  public String getAccountName() {
+    return accountName;
+  }
+
+  public int getAccountNumber() {
+    return ACCOUNT_NUMBER;
+  }
+
+  public Double getInitialBalance() {
+    return initialBalance;
+  }
+
+  public Double getBalance() {
+    return balance;
+  }
+
+  public static Double getMonthlyInterestRate() {
+    monthlyInterestRate = annualInterestRate / 12;
+    return monthlyInterestRate;
+  }
+
+  public static Double getAnnualInterestRate() {
+    return annualInterestRate;
+  }
 
 
+  public static void  setAnnualInterestRate(Double rate) {
+    annualInterestRate = rate;
+  }
+
+  SavingsAccount(String accountName, int accountNumber, Double initialBalance) {
+    this.accountName = accountName;
+    this.ACCOUNT_NUMBER = accountNumber;
+    this.initialBalance = initialBalance;
+  }
+
+  // overload
+  SavingsAccount(int accountNumber, Double initialBalance) {
+    this.ACCOUNT_NUMBER = accountNumber;
+    this.initialBalance = initialBalance;
+  }
+
+  public Double addMonthlyInterest(){
     
+    return getBalance(); 
+
+  }
+
 }
--- SavingsAccountDriver.java
+++ SavingsAccountDriver.java
@@ -1,11 +1,26 @@
 package OOP_A1;
 
-import static java.lang.System.*; 
+import static java.lang.System.*;
 
 public class SavingsAccountDriver {
-    
-    public static void main (String [] args) {
 
-        out.println("test");
+    public static void main(String[] args) {
+
+
+        SavingsAccount saver1 = new SavingsAccount(10002, (double) 2000);
+        SavingsAccount saver2 = new SavingsAccount(10003, (double) 3000);
+
+        out.println(saver1.getAccountNumber());
+        out.println(Helper.formatTwoDecimals(saver1.getInitialBalance()));
+        out.println(saver2.getAccountNumber());
+        out.println(Helper.formatfourDecimals(SavingsAccount.getMonthlyInterestRate()));
+        out.println(Helper.formatfourDecimals(SavingsAccount.getAnnualInterestRate()));
+        
+        SavingsAccount.setAnnualInterestRate(0.10);
+        out.println(Helper.formatfourDecimals(SavingsAccount.getMonthlyInterestRate()));
+        out.println(Helper.formatfourDecimals(SavingsAccount.getAnnualInterestRate()));
+        out.println("----");
+        out.println(saver1.getInitialBalance());
+        out.println(saver1.getBalance());
     }
 }
