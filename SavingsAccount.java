28e31666c7ba5b3906d19c53644750d21fc03166
Eddie C. <edimc2000@gmail.com>
2025-11-01 01:56:45 -0500

    init commit

---
+++ SavingsAccount.java
@@ -0,0 +1,12 @@
+
+package OOP_A1;
+
+import static java.lang.System.out; 
+
+public class SavingsAccount {
+
+  out.println("tesu");
+
+
+    
+}
---
+++ SavingsAccountDriver.java
@@ -0,0 +1,11 @@
+package OOP_A1;
+
+import static java.lang.System.*; 
+
+public class SavingsAccountDriver {
+    
+    public static void main (String [] args) {
+
+        out.println("test");
+    }
+}
